77909b75fe10d4cce9b26983072cfb0dc191b46f
fhaselbeck <florian.haselbeck@hswt.de>
2023-01-20 13:39:33 +0100

    initial commit

--- prolatherm/run_prolatherm.py
+++ prolatherm/run_prolatherm.py
@@ -1,80 +1,73 @@
-import sklearn.metrics
-import numpy as np
 import pandas as pd
+import pathlib
+import argparse
+import utils
+from tqdm import tqdm
 
 import prolatherm
 
 
-def get_evaluation_report(y_pred: np.array, y_true: np.array, y_score: np.array, task: str, prefix: str = '') -> dict:
-    """
-    Get values for common evaluation metrics
-
-    :param y_pred: predicted values
-    :param y_true: true values
-    :param y_score: scores for predict value
-    :param task: ML task to solve
-    :param prefix: prefix to be added to the key if multiple eval metrics are collected
-
-    :return: dictionary with common metrics
-    """
-    if len(y_pred) == (len(y_true)-1):
-        print('y_pred has one element less than y_true (e.g. due to batch size config) -> dropped last element')
-        y_true = y_true[:-1]
-    if task == 'classification':
-        average = 'micro' if len(np.unique(y_true)) > 2 else 'binary'
-        #roc_fpr, roc_tpr, roc_thr = sklearn.metrics.roc_curve(y_true=y_true, y_score=y_score)
-        eval_report_dict = {
-            prefix + 'accuracy': sklearn.metrics.accuracy_score(y_true=y_true, y_pred=y_pred),
-            prefix + 'f1_score': sklearn.metrics.f1_score(y_true=y_true, y_pred=y_pred, average=average),
-            prefix + 'precision': sklearn.metrics.precision_score(y_true=y_true, y_pred=y_pred,
-                                                                  zero_division=0, average=average),
-            prefix + 'recall': sklearn.metrics.recall_score(y_true=y_true, y_pred=y_pred,
-                                                            zero_division=0, average=average),
-            prefix + 'mcc': sklearn.metrics.matthews_corrcoef(y_true=y_true, y_pred=y_pred)
-            #prefix + 'roc_list_fpr': roc_fpr,
-            #prefix + 'roc_list_tpr': roc_tpr,
-            #prefix + 'roc_list_threshold': roc_thr,
-            #prefix + 'roc_auc': sklearn.metrics.roc_auc_score(y_true=y_true, y_score=y_score)
-        }
-    else:
-        eval_report_dict = {
-            prefix + 'mse': sklearn.metrics.mean_squared_error(y_true=y_true, y_pred=y_pred),
-            prefix + 'rmse': sklearn.metrics.mean_squared_error(y_true=y_true, y_pred=y_pred, squared=False),
-            prefix + 'r2_score': sklearn.metrics.r2_score(y_true=y_true, y_pred=y_pred),
-            prefix + 'explained_variance': sklearn.metrics.explained_variance_score(y_true=y_true, y_pred=y_pred)
-        }
-    return eval_report_dict
-
-
 if __name__ == '__main__':
-    raw_data = pd.read_csv("/myhome/ProLaTherm/data/datasets_w_datasplits/ProtThermPred_speciesspecific.csv")
-    unique_data = raw_data.iloc[4797:]
-    prot_ids_test = unique_data["meta_protein_id"]
-    seqs_test = unique_data['seq_peptide']
-    y_test = unique_data['label_binary']
+
+    parser = argparse.ArgumentParser()
+    base_path = pathlib.Path().absolute()
+    # Input Params #
+    parser.add_argument("-df", "--data_dir_fasta", type=str,
+                        default=base_path.joinpath('assets', 'dummy_data.fasta'),
+                        help="Provide the full path to the fasta file you want to get predictions for."
+                             "Default is the dummy_data.fasta we provide in prolatherm/assets")
+    parser.add_argument("-sd", "--save_dir", type=str, default=base_path.parent,
+                        help="Provide the full path of the directory in which you want to save your results. "
+                             "Default is in the root directory of your repository.")
+    args = vars(parser.parse_args())
+    data_dir_fasta = pathlib.Path(args["data_dir_fasta"])
+    save_dir = pathlib.Path(args["save_dir"])
+
+    # Check input arguments
+    print("Checking input arguments")
+    if not data_dir_fasta.is_file():
+        raise Exception("Specified fasta file does not exist: " + str(data_dir_fasta))
+    if not save_dir.exists():
+        raise Exception("Specified save directory does not exist: " + str(save_dir))
+    print("Input arguments valid")
+
+    # Read fasta file
+    print("Reading fasta file " + str(data_dir_fasta))
+    sequences, protein_ids = utils.read_fasta(data_dir_fasta)
+    if len(sequences) == 0:
+        raise Exception("Fasta file is empty or does not match format")
+
+    # Load prediction model
+    print("Load ProLaTherm model")
     pred_model = prolatherm.ProLaTherm()
 
+    # Run prediction pipeline in batches of size 10
     start = 0
     step_size = 10
-    num_samples = unique_data.shape[0]
+    num_samples = len(sequences)
     preds = []
     scores = []
-    for i in range(step_size, num_samples, step_size):
+    for i in tqdm(range(step_size, num_samples, step_size)):
         end = i if i + step_size < num_samples else num_samples
-        print(start)
-        print(end)
-        # prot_ids = prot_ids_test[start:end]
-        seq = seqs_test[start:end]
+        seqs = sequences[start:end]
+        if start+1 == end:
+            seqs = [seqs]
         print("Running prediction")
-        print(seq)
-        pred, score = pred_model.predict(seq)
-        print(pred)
-        print(score)
+        pred, score = pred_model.predict(seqs)
         preds.extend(pred.flatten().tolist())
         scores.extend(score.flatten().tolist())
         start = end
-    print(get_evaluation_report(y_pred=np.array(preds), y_score=np.array(scores), y_true=np.array(y_test), task="classification"))
+    print("Finished. Creating results file.")
 
-    # fasta file laden, das als param übergeben werden kann
-    # daten batchen und predictions machen
-    # csv abspeichern
+    # Save results
+    results = pd.DataFrame(columns=["IDs", "prediction_binary", "prediction", "score"])
+    results["IDs"] = protein_ids
+    results["prediction_binary"] = preds
+    results["score"] = scores
+    results["prediction"] = \
+        results["prediction_binary"].apply(lambda x: "thermophilic" if x == 1 else "non-thermophilic")
+    results_file_path = save_dir.joinpath("ProLaTherm_Predictions_" + str(data_dir_fasta[-1]).replace(".fasta", ".csv"))
+    results.to_csv(results_file_path, index=False)
+    print("Saved results at: " + str(results_file_path))
+    print("Here are the final predictions:")
+    print(results)
--- prolatherm/utils.py
+++ prolatherm/utils.py
@@ -1,6 +1,8 @@
 import torch
 import random
 import numpy as np
+import pathlib
+from itertools import groupby
 
 
 def set_all_seeds(seed: int = 42):
@@ -17,3 +19,32 @@ def set_all_seeds(seed: int = 42):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+
+def read_fasta(filepath: pathlib.Path) -> (np.array, np.array):
+    """
+    Read protein FASTA files (.fasta, .txt), i.e. files that contain pais of protein ID and amino aid sequence:
+
+    >protein1|some identifier|another identifier|
+    MGKRVVIALGGNALQQRGQKGSYEEMMDNVRKTARQIAEIIARGYEVVIT
+    MSTESEIAVRIRGIYSTALTKLLMDRGFKIVQPSDVIAERFGIEKSYEDF
+    DVDIYDKNHGVTIVGTKVEAVKKVFEEEFIDVFFRKLPYKLHGIYKGLVV
+    KRDDRFVYVDIGNVIGTVLIEELPDAAEGDEVVVQVKKHNVLPHLSTLIT
+    >protein2|some identifier|another identifier|
+    GLEDVYIDQTNICYIDGKEGKLYYRGYSVEELAELSTFEEVVYLLEIIAE
+
+    Will return sequences and corresponding IDs (= the complete first line without '>')
+
+    :param filepath: full path of FASTA file
+
+    :return: sequences and corresponding IDs in two separate numpy arrays
+    """
+    with open(filepath, "r") as f:
+        sequences = []
+        protein_ids = []
+        groups = (x[1] for x in groupby(f, lambda line: line[0] == ">"))
+        for header in groups:
+            pid = header.__next__().replace('>', '').strip()
+            seq = "".join(s.strip() for s in groups.__next__())  # join all sequence lines to one.
+            protein_ids.append(pid)
+            sequences.append(seq)
+    return np.array(sequences), np.array(protein_ids)
