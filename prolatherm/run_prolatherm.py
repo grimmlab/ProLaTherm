810e91eeb3b3cb482cb6a3692a85f8c83126e877
fhaselbeck <florian.haselbeck@hswt.de>
2023-01-20 11:44:29 +0100

    initial commit

--- prolatherm/run_prolatherm.py
+++ prolatherm/run_prolatherm.py
@@ -22,7 +22,7 @@ def get_evaluation_report(y_pred: np.array, y_true: np.array, y_score: np.array,
         y_true = y_true[:-1]
     if task == 'classification':
         average = 'micro' if len(np.unique(y_true)) > 2 else 'binary'
-        roc_fpr, roc_tpr, roc_thr = sklearn.metrics.roc_curve(y_true=y_true, y_score=y_score)
+        #roc_fpr, roc_tpr, roc_thr = sklearn.metrics.roc_curve(y_true=y_true, y_score=y_score)
         eval_report_dict = {
             prefix + 'accuracy': sklearn.metrics.accuracy_score(y_true=y_true, y_pred=y_pred),
             prefix + 'f1_score': sklearn.metrics.f1_score(y_true=y_true, y_pred=y_pred, average=average),
@@ -30,11 +30,11 @@ def get_evaluation_report(y_pred: np.array, y_true: np.array, y_score: np.array,
                                                                   zero_division=0, average=average),
             prefix + 'recall': sklearn.metrics.recall_score(y_true=y_true, y_pred=y_pred,
                                                             zero_division=0, average=average),
-            prefix + 'mcc': sklearn.metrics.matthews_corrcoef(y_true=y_true, y_pred=y_pred),
-            prefix + 'roc_list_fpr': roc_fpr,
-            prefix + 'roc_list_tpr': roc_tpr,
-            prefix + 'roc_list_threshold': roc_thr,
-            prefix + 'roc_auc': sklearn.metrics.roc_auc_score(y_true=y_true, y_score=y_score)
+            prefix + 'mcc': sklearn.metrics.matthews_corrcoef(y_true=y_true, y_pred=y_pred)
+            #prefix + 'roc_list_fpr': roc_fpr,
+            #prefix + 'roc_list_tpr': roc_tpr,
+            #prefix + 'roc_list_threshold': roc_thr,
+            #prefix + 'roc_auc': sklearn.metrics.roc_auc_score(y_true=y_true, y_score=y_score)
         }
     else:
         eval_report_dict = {
